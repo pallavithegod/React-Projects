e51cc9f2cd2159507422ff3200fd9c5ca2659e6a
unknown <jainpallavi.delhi@gmail.com>
2025-06-21 22:46:06 +0530

    content added

--- firstreactproj/src/main.jsx
+++ firstreactproj/src/main.jsx
@@ -1,9 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx'  //start a function's name from capital letter
+import Test from './Test.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById('root'))
+.render(
+  // render function takes html only as an input and parses it to js object form
+  <>    
+  {/* since single fragment can be rendered thus, enclosed within div or empty tag*/}
+    <h3>rendered (directly) without function</h3>
+    <Test />
+    <CustomFxn/>
+  </>
 )
+
+function CustomFxn(){
+  return(
+    <div>
+      <p>function rendered directly inside main</p>
+    </div>
+  )
+}
