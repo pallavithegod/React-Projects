ac82e1fa527ac29a9b67af1ad7d88849625d84b9
unknown <jainpallavi.delhi@gmail.com>
2025-07-24 23:52:25 +0530

    add routes in main

--- router-project/src/main.jsx
+++ router-project/src/main.jsx
@@ -1,8 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, RouterProvider } from "react-router";
+import { BrowserRouter, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router";
 import './index.css'
 
+import Files from './Files'
+import Contact from './Components/Contact/Contact';
+
+const router = createBrowserRouter(
+   createRoutesFromElements(
+      <Route path = "/" element = {<Layout />}>
+         <Route path = "/about" element = {<About />} />
+         <Route path = "/contact" element = {<Contact />} />
+         <Route path = "/github" element = {<Github />} />
+         <Route path = "/user" element = {<User />} />
+         <Route path = "/home" element = {<Home />} />
+      </Route>
+   )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
