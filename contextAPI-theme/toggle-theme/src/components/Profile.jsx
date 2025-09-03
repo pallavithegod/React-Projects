e85b035ea4a76d322e8a1e0afacb11a948eff7aa
pallavithegod <jainpallavi.delhi@gmail.com>
2025-09-03 23:57:59 +0530

    modified

--- contextAPI-theme/toggle-theme/src/components/Profile.jsx
+++ contextAPI-theme/toggle-theme/src/components/Profile.jsx
@@ -3,9 +3,9 @@
  
  function Profile() {
   const {user} = useContext(UserContext)
-    
-  
+
    if(!user) return <div> <br/> Please Login</div>
+
    return <div> <br/>Welcome {user.username}</div>
  }
  
